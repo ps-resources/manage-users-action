9e98190d3c7f59946fa642ced4fc4c136da7da24
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2022-10-12 19:35:16 +0200

    Add logic

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,58 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { getInput, info, setFailed } from '@actions/core'
+import { context, getOctokit } from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const users: string[] = getInput('users').split(',')
+    for (const user of users) {
+      info(`user: ${user}`)
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const repositories: string[] = getInput('repositories').split(',')
+    for (const repository of repositories) {
+      info(`repository: ${repository}`)
+    }
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    const role: string = getInput('role')
+    info(`role: ${role}`)
+
+    const token = getInput('token')
+    const issueNumber = context.payload.issue && context.payload.issue.number
+    const octokit = getOctokit(token)
+
+    const promises: Promise<any>[] = []
+
+    for (const repository of repositories) {
+      for (const user of users) {
+        promises.push(
+          octokit.rest.repos.addCollaborator({
+            owner: context.payload.organization.login,
+            repo: repository,
+            username: user,
+            permission: role === 'write' ? 'push' : 'pull',
+          }),
+        )
+      }
+    }
+
+    const result = await Promise.all(promises)
+    info(JSON.stringify(result))
+
+    let message = ''
+    for (const response of result) {
+      message += `Added ${response.username} to ${response.repo} with ${response.permission} permissions\n`
+    }
+
+    octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: issueNumber as number,
+      body: message,
+    })
+  } catch (error: any) {
+    setFailed(error.message)
   }
 }
 
 run()
+
+//write a function to get actions billing information from the github api
