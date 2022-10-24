21c713955b32bc798bd829dc9b1d8d5e06d7d235
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2022-10-24 14:57:51 +0200

    Simplify logic to delegate to workflow

--- src/main.ts
+++ src/main.ts
@@ -23,23 +23,17 @@ async function run(): Promise<void> {
     const action: string = getInput('action')
     info(`action: ${action}`)
 
-    let message = ''
     for (const repository of repositories) {
       for (const user of users) {
         if (action === 'add') {
+          info(`Adding ${user} to ${repository} with role ${role}`)
           const response = await octokit.rest.repos.addCollaborator({
             owner: repository.split('/')[0],
             repo: repository.split('/')[1],
             username: user,
             permission: role === 'write' ? 'push' : 'pull'
           })
-          
-          if (response.status === 201)
-            message += `Added @${user} to [${repository}](https://github.com/${repository}) with \`${role}\` access`
-          else
-            message += `Failed to add @${user} to [${repository}](https://github.com/${repository}) with \`${role}\` access.`
         } else if (action === 'remove') {
-          try {
             const isCollaborator = await octokit.rest.repos.checkCollaborator({
               owner: repository.split('/')[0],
               repo: repository.split('/')[1],
@@ -47,61 +41,33 @@ async function run(): Promise<void> {
             })
           
             if (isCollaborator.status === 204) {
-              const response = await octokit.rest.repos.removeCollaborator({
+              info(`Removing ${user} from ${repository}`)
+              await octokit.rest.repos.removeCollaborator({
                 owner: repository.split('/')[0],
                 repo: repository.split('/')[1],
                 username: user
               })
-
-              if (response.status === 204)
-                message += `Removed @${user} from [${repository}](https://github.com/${repository})`
-              else
-                message += `Failed to remove @${user} from [${repository}](https://github.com/${repository})`
             } else {
-              message += `@${user} is not a collaborator on [${repository}](https://github.com/${repository})`
-            }
-          } catch (error) {
-            const invitations = await octokit.rest.repos.listInvitations({
-              owner: repository.split('/')[0],
-              repo: repository.split('/')[1]
-            })
-
-            const invitation = invitations.data.find(invitation => invitation.invitee?.login === user)
-            if (invitation) {
-              const response = await octokit.rest.repos.deleteInvitation({
+              const invitations = await octokit.rest.repos.listInvitations({
                 owner: repository.split('/')[0],
-                repo: repository.split('/')[1],
-                invitation_id: invitation.id
+                repo: repository.split('/')[1]
               })
-              message += `Cancelled invitation of @${user} from [${repository}](https://github.com/${repository})`
-            } else {
-              message += `No invitation of @${user} from [${repository}](https://github.com/${repository})`
+
+              const invitation = invitations.data.find(invitation => invitation.invitee?.login === user)
+              if (invitation) {
+                info(`Cancelling invitation for ${user} to ${repository}`)
+                await octokit.rest.repos.deleteInvitation({
+                  owner: repository.split('/')[0],
+                  repo: repository.split('/')[1],
+                  invitation_id: invitation.id
+                })
+              }
             }
-          }
         } else { 
           throw new Error('Action must be add or remove')
         }
       }
     }
-
-    octokit.rest.issues.createComment({
-      ...context.repo,
-      issue_number: issueNumber as number,
-      body: message
-    })
-
-    const label = action === 'add' ? 'added' : 'removed'
-    octokit.rest.issues.addLabels({
-      ...context.repo,
-      issue_number: issueNumber as number,
-      labels: [label]
-    })
-
-    octokit.rest.issues.update({
-      ...context.repo,
-      issue_number: issueNumber as number,
-      state: 'closed'
-    })
   } catch (error) {
     if (error instanceof Error) {
       const issue = await octokit.rest.issues.get({
