52495f1beea8e054c94e7083757c06e4f0cd0f26
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2022-11-07 13:52:12 +0100

    Add validation to better handle non-existing repositories or users

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
 import {error, getInput, info, setFailed} from '@actions/core'
 import {getOctokit} from '@actions/github'
 
+async function validate(token: string, users: string[], repositories: string[]): Promise<void> {
+  const octokit = getOctokit(token)
+
+  let invalid = false
+  let errorMessage = ''
+  for (const user of users) {
+    const userExists = await octokit.rest.users.getByUsername({username: user})
+    if (userExists.status !== 200) {
+      invalid = true
+      errorMessage += `User ${user} does not exist.\n`
+    }
+  }
+
+  for (const repository of repositories) {
+    const repositoryExists = await octokit.rest.repos.get({
+      owner: repository.split('/')[0],
+      repo: repository.split('/')[1]
+    })
+    if (repositoryExists.status !== 200) {
+      invalid = true
+      errorMessage += `Repository ${repository} does not exist.\n`
+    }
+  }
+
+  if (invalid) {
+    error(errorMessage)
+    setFailed(errorMessage)
+  }
+}
+
 async function run(): Promise<void> {
   const token = getInput('token', {required: true})
-  const octokit = getOctokit(token)
 
   try {
     const users: string[] = getInput('users').replace(/\s/g, '').split(',')
@@ -21,6 +50,10 @@ async function run(): Promise<void> {
 
     const action: string = getInput('action')
     info(`action: ${action}`)
+
+    await validate(token, users, repositories)
+
+    const octokit = getOctokit(token)
 
     for (const repository of repositories) {
       for (const user of users) {
